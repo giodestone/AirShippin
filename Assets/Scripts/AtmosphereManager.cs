c5ea465ed5b7a894c6f6cb07817faf6f4d71fd43
explosiveeraser <aanderson021099@gmail.com>
2021-07-12 21:10:51 +0100

    Updated from previous commit.

--- Assets/Scripts/AtmosphereManager.cs
+++ Assets/Scripts/AtmosphereManager.cs
@@ -9,18 +9,6 @@ public class AtmosphereManager : MonoBehaviour
     const float RefHeight = 0f;
     const float RefPressure = 101325f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     static float AmbientTemperature(float height)
     {
         return RefTemperature - 0.0065(height - RefHeight)
@@ -30,6 +18,5 @@ public class AtmosphereManager : MonoBehaviour
     {
         return  Math.Pow(RefPressure(1-(0.0065*height)/(AmbientTemperature + 0.0065*height)), 5.257)
     }
-
 
 }
