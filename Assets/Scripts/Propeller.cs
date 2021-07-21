2e30e9f1c7c97b9e57a52b1d18f38e88555ff449
explosiveeraser <74459165+explosiveeraser@users.noreply.github.com>
2021-07-21 22:37:18 +0100

    Added Thrust.

--- Assets/Scripts/Propeller.cs
+++ Assets/Scripts/Propeller.cs
@@ -6,7 +6,7 @@ public class Propeller : MonoBehaviour
 {
     public Rigidbody rb;
 
-    private float maxThrust = 2200f;
+    private float maxThrust = 6000f;
     private float velocityScaleConst = 100f;
     private float AppliedThrust;
     AirshipFuelCanisterItemHolder FuelInUse;
